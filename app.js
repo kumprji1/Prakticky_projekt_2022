119dc18808dc503ee7ee7801c7c394778888d3e6
Jiri <kalendar.skolavrchlabi@gmail.com>
2022-03-23 21:43:37 +0100

    env

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const express = require('express')
 
 const app = express()
 
+// Enabling .env variables
+const dotenv = require('dotenv');
+dotenv.config();
+
 app.set('views', 'views')
 app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(3000)
+app.listen(process.env.PORT)
 
 app.use((req, res, next) => {
     res.render('firstWorkshop')
